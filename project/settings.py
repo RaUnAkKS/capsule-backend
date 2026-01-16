7a3800ffce30d67c16b5cd18923e030550b3d275
RaUnAkKS <raunaksaraf6@gmail.com>
2026-01-17 02:29:14 +0530

    Add cloudinary support

--- project/settings.py
+++ project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'app',
     'rest_framework',
     'corsheaders',
+    'cloudinary_storage', # Add this
+    'cloudinary',          # Add this
 ]
 
 MIDDLEWARE = [
@@ -127,6 +129,16 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Cloudinary Configuration
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 from datetime import timedelta
 
 SIMPLE_JWT = {
